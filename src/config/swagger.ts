72e985ec1c781c601b778f95d00d761431aa0990
phucnh17 <phucnh17@msb.com.vn>
2024-07-06 01:14:35 +0700

    feat: init base

---
+++ src/config/swagger.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export const SwaggerConfig = (app: INestApplication, serviceName: string) => {
+  const config = new DocumentBuilder()
+    .setTitle(`${serviceName}`)
+    .setDescription(`The ${serviceName} collection api`)
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${serviceName}/api`, app, document);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerConfig } from './config/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService)
+
+  SwaggerConfig(app, config.get<string>('APP_PREFIX'))
+
   await app.listen(3000);
 }
 bootstrap();
