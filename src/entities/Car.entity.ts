4958947e903b09c1e89989fba45ccdbab89a18ea
phucnh17 <phucnh17@msb.com.vn>
2024-07-06 14:28:21 +0700

    feat: init base

--- src/entities/Car.entity.ts
+++ src/entities/Car.entity.ts
@@ -1,5 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { Base } from "./Base.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity('car')
 export class Car extends Base {
@@ -7,8 +8,10 @@ export class Car extends Base {
   id: number
 
   @Column()
+  @ApiProperty({ default: 'honda-civic' })
   name: string
 
   @Column()
+  @ApiProperty({ default: '5000' })
   price: string
 }
