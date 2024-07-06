e92f65595e25480d4a925dd7792ebfd98da2d435
phucnh17 <phucnh17@msb.com.vn>
2024-07-06 20:43:24 +0700

    feat: init base

--- src/entities/Image.entity.ts
+++ src/entities/Image.entity.ts
@@ -1,11 +1,16 @@
-import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, Relation } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, Relation } from "typeorm";
 import { Base } from "./Base.entity";
 import { Product } from "./Product.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity('image')
 export class Image extends Base {
   @PrimaryGeneratedColumn()
   id: number
+
+  @Column()
+  @ApiProperty({ default: 'http://google.com' })
+  url: string
 
   @ManyToOne(() => Product, (product) => product.images)
   @JoinColumn()
