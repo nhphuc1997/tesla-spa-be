ccf4b20bbd278ede2dadcf41460e0253240d0ea2
phucnh17 <phucnh17@msb.com.vn>
2024-07-06 20:11:39 +0700

    feat: init base

--- src/modules/color-group/color-group.module.ts
+++ src/modules/color-group/color-group.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ColorGroupService } from './color-group.service';
 import { ColorGroupController } from './color-group.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ColorGroup } from 'src/entities/ColorGroup.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ColorGroup])
+  ],
   controllers: [ColorGroupController],
   providers: [ColorGroupService],
 })
